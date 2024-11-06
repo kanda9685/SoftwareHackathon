4ead75d914ebef9d73c270e9373f1634aff56716
oyokoi451 <meronpan3690@gmail.com>
2024-11-07 01:32:58 +0900

    add language_config

--- menu_app/lib/screens/camera_screen.dart
+++ menu_app/lib/screens/camera_screen.dart
@@ -11,13 +11,13 @@ import 'package:menu_app/models/menu_item.dart';
 
 class CameraScreen extends StatefulWidget {
   final CameraDescription camera;
-  final Function(List<MenuItem>) updateMenuItems;
+  final Function(List<MenuItem>) addMenuItems;
   final Function(int) updateIndex;
 
   const CameraScreen({
     super.key, 
     required this.camera, 
-    required this.updateMenuItems,
+    required this.addMenuItems,
     required this.updateIndex,
   });
 
@@ -28,6 +28,7 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
+  String _selectedLanguage = 'English'; // 初期言語設定
 
   List<MenuItem> menuItems = [];
 
@@ -50,8 +51,8 @@ class _CameraScreenState extends State<CameraScreen> {
 
   Future<void> uploadImage(String imagePath) async {
     final file = await rotateAndSaveImage(File(imagePath));  // utils/image_utils.dart のメソッドを使用して画像を回転して保存
-    // final uploadUrl = 'http://localhost:8888/process_menus'; // 適切なURLに変更
-    final uploadUrl = 'http://192.168.10.111:8000/process_menus'; // 神田、開発用
+    //final uploadUrl = 'http://192.168.10.111:8000/process_menus'; // 神田、開発用
+    final uploadUrl = 'http://172.16.0.178:8000/process_menus'; // 横井、開発用
 
 
     // 通信中ダイアログを表示
@@ -64,7 +65,7 @@ class _CameraScreenState extends State<CameraScreen> {
             children: [
               CircularProgressIndicator(),
               SizedBox(width: 20),
-              Text("通信中..."),
+              Text("Loading..."),
             ],
           ),
         );
@@ -84,7 +85,7 @@ class _CameraScreenState extends State<CameraScreen> {
 
         List<MenuItem> items = results.map((item) => MenuItem.fromJson(item)).toList();
 
-        widget.updateMenuItems(items);
+        widget.addMenuItems(items);
         widget.updateIndex(1); // メニュー一覧画面に遷移
       } else {
         // アップロード失敗時の処理
@@ -99,35 +100,100 @@ class _CameraScreenState extends State<CameraScreen> {
     }
   }
 
-
-  Widget _buildCameraScreen() {
-    return Column(
-      children: [
-        FutureBuilder<void>(
-          future: _initializeControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return Expanded(child: CameraPreview(_controller));
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          },
-        ),
-        FloatingActionButton(
-          onPressed: () async {
-            try {
-              await _initializeControllerFuture;
-              final image = await _controller.takePicture();
-              await uploadImage(image.path);
-            } catch (e) {
-              print(e);
-            }
-          },
-          child: const Icon(Icons.camera_alt),
-        ),
-      ],
+  void _showLanguageDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Select Language"),
+          content: Container(
+            width: double.maxFinite,
+            child: ListView(
+              shrinkWrap: true,
+              children: <String>['English', 'Chinese', 'Korean']
+                  .map((String language) {
+                return ListTile(
+                  title: Text(language),
+                  trailing: _selectedLanguage == language
+                      ? Icon(Icons.check, color: Colors.blue) // 選択済みにチェック
+                      : null,
+                  onTap: () {
+                    setState(() {
+                      _selectedLanguage = language;
+                    });
+                    Navigator.of(context).pop();
+                  },
+                );
+              }).toList(),
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // キャンセルボタンでダイアログを閉じる
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        );
+      },
     );
   }
+
+  void fetchImages() {
+
+  }
+
+  Widget _buildCameraScreen() {
+  return Column(
+    children: [
+      Expanded(
+      child:Stack(
+        fit: StackFit.passthrough,
+        children: [
+          // カメラプレビュー
+          FutureBuilder<void>(
+            future: _initializeControllerFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return Expanded(child: CameraPreview(_controller));
+              } else {
+                return const Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+          // 設定用の半透明ボタンを右下に配置
+          Positioned(
+            right: 5,
+            bottom: 5, // 撮影ボタンとの位置調整
+            child: Opacity(
+              opacity: 0.7, // 半透明に設定
+              child: FloatingActionButton(
+                onPressed: _showLanguageDialog,
+                child: const Icon(Icons.settings),
+              ),
+            ),
+          ),
+        ],
+      ),
+      ),
+      // 撮影ボタン
+      FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+            final image = await _controller.takePicture();
+            await uploadImage(image.path);
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    ],
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
