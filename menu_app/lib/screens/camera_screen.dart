d272169b0ce0da2d258eb81eb880c25d2b9775aa
kanda <kanda413@icloud.com>
2024-11-15 19:28:54 +0900

    Modify

--- menu_app/lib/screens/camera_screen.dart
+++ menu_app/lib/screens/camera_screen.dart
@@ -10,7 +10,10 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:menu_app/models/menu_item.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:image_picker/image_picker.dart';
+// 画像選択用のインポートを変更
+import 'package:image_picker_web/image_picker_web.dart'; // Web用のimage_picker_webをインポート
+import 'package:flutter/foundation.dart'; // kIsWebを使用するために必要
+
 import 'dart:io';
 
 
@@ -78,29 +81,49 @@ class _CameraScreenState extends State<CameraScreen> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       // ギャラリーの選択ボタン
+                      // ギャラリーの選択ボタン部分の変更
+                      // カメラボタンの処理変更（Webではカメラは非対応の場合）
                       GestureDetector(
                         onTap: () async {
-                          // ギャラリーから画像を選択
-                          final picker = ImagePicker();
-                          final pickedFile = await picker.pickImage(source: ImageSource.gallery);
-                          
-                          if (pickedFile != null) {
-                            await Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => DisplayPictureScreen(
-                                  imagePath: pickedFile.path,
-                                  addMenuItems: widget.addMenuItems,
-                                  updateIndex: widget.updateIndex,
+                          if (kIsWeb) {
+                            // Webではカメラ機能は使用せず、画像を選択させる
+                            final pickedFile = await ImagePickerWeb.getImageAsBytes(); // Web用の画像選択
+                            if (pickedFile != null) {
+                              await Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => DisplayPictureScreen(
+                                    image: '', // 画像データがバイト形式で渡される場合はそのまま利用
+                                    addMenuItems: widget.addMenuItems,
+                                    updateIndex: widget.updateIndex,
+                                  ),
                                 ),
-                              ),
-                            );
+                              );
+                            }
+                          } else {
+                            // モバイル用のカメラ処理（既存の処理）
+                            try {
+                              await _initializeControllerFuture;
+                              final image = await _controller.takePicture();
+                              await Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => DisplayPictureScreen(
+                                    image: image,
+                                    addMenuItems: widget.addMenuItems,
+                                    updateIndex: widget.updateIndex,
+                                  ),
+                                ),
+                              );
+                            } catch (e) {
+                              print(e);
+                            }
                           }
                         },
                         child: Container(
-                          padding: const EdgeInsets.all(12),
+                          width: 70,
+                          height: 70,
                           decoration: BoxDecoration(
                             color: Colors.white,
-                            borderRadius: BorderRadius.circular(12),
+                            shape: BoxShape.circle,
                             boxShadow: [
                               BoxShadow(
                                 color: Colors.black.withOpacity(0.3),
@@ -109,15 +132,10 @@ class _CameraScreenState extends State<CameraScreen> {
                               ),
                             ],
                           ),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: const [
-                              Icon(
-                                Icons.photo_library, // ギャラリーアイコン
-                                size: 30,
-                                color: Colors.black,
-                              ),
-                            ],
+                          child: const Icon(
+                            Icons.camera_alt,
+                            size: 40,
+                            color: Colors.black,
                           ),
                         ),
                       ),
@@ -131,7 +149,7 @@ class _CameraScreenState extends State<CameraScreen> {
                             await Navigator.of(context).push(
                               MaterialPageRoute(
                                 builder: (context) => DisplayPictureScreen(
-                                  imagePath: image.path,
+                                  image: image,
                                   addMenuItems: widget.addMenuItems,
                                   updateIndex: widget.updateIndex,
                                 ),
@@ -182,13 +200,13 @@ class _CameraScreenState extends State<CameraScreen> {
 }
 
 class DisplayPictureScreen extends StatefulWidget {
-  final String imagePath;
+  final dynamic image; // Webの場合はバイトデータ、モバイルの場合はファイルパス
   final Function(List<MenuItem>) addMenuItems;
   final Function(int) updateIndex;
 
   const DisplayPictureScreen({
     super.key,
-    required this.imagePath,
+    required this.image, // Webの場合はバイトデータ
     required this.addMenuItems,
     required this.updateIndex,
   });
@@ -259,11 +277,9 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
         return  {"Latitude": position.latitude, "Longitude": position.longitude};
   }
 
-  Future<void> uploadImage(String imagePath) async {
+  Future<void> uploadImage(dynamic image) async {
     String selectedLanguage = Provider.of<LanguageProvider>(context, listen: false).selectedLanguage;
 
-    final file = await rotateAndSaveImage(File(imagePath));
-    // final uploadUrl = 'https://menubite.proudpebble-333036c6.australiaeast.azurecontainerapps.io/process_menus';
     final uploadUrl = 'http://192.168.10.111:8000/process_menus';
 
     showDialog(
@@ -290,19 +306,32 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
         'POST',
         Uri.parse(uploadUrl),
       );
-    
+
+      // 位置情報と選択された言語をフィールドとして追加
       request.fields['lat'] = gps_info["Latitude"].toString();
       request.fields['lng'] = gps_info["Longitude"].toString();
       request.fields['language'] = selectedLanguage;
 
       print(request);
-      request.files.add(await http.MultipartFile.fromPath('file', file.path));
+
+      // Webの場合はバイトデータを使用してMultipartFileを作成
+      if (kIsWeb) {
+        // imageはバイトデータなので、直接送信
+        request.files.add(http.MultipartFile.fromBytes(
+          'file',
+          image,
+          filename: 'image.jpg',  // 任意のファイル名を指定
+        ));
+      } else {
+        // モバイルの場合は、ファイルパスを指定して画像を送信
+        final file = await rotateAndSaveImage(File(image));  // File(image) -> File(path)
+        request.files.add(await http.MultipartFile.fromPath('file', file.path));
+      }
 
       print(request.files);
 
       final response = await request.send().timeout(const Duration(seconds: 100));
 
-      print(selectedLanguage);
       Navigator.of(context, rootNavigator: true).pop(); // 通信中ダイアログを閉じる
 
       if (response.statusCode == 200) {
@@ -335,20 +364,20 @@ class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          Provider.of<LanguageProvider>(context).getLocalizedString('Preview')
+          Provider.of<LanguageProvider>(context).getLocalizedString('Preview'),
         ),
       ),
       body: Center(
         child: FittedBox(
           fit: BoxFit.contain, // アスペクト比を維持して画面全体に合わせる
-          child: Image.file(
-            File(widget.imagePath),
+          child: Image.memory(
+            widget.image, // Webではバイトデータを使用
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          await uploadImage(widget.imagePath);
+          await uploadImage(widget.image); // Webの場合はバイトデータを渡す
         },
         child: const Icon(Icons.send),
       ),
