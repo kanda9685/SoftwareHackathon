09875303161c0b6f09c1eaf9d2291bdc08fd132c
oyokoi451 <meronpan3690@gmail.com>
2025-02-10 00:30:28 +0900

    modify config.py to use .env in Docker Container

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -1,8 +1,12 @@
 import os
+from os.path import join, dirname
 from dotenv import load_dotenv
 
 # 環境変数を読み込む
-load_dotenv()
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+print(dotenv_path)
+load_dotenv(dotenv_path)
 
 CHATGPT_API_KEY = os.getenv("CHATGPT_API_KEY") # ChatGPT API の Key
 GOOGLE_SEARCH_API_KEY = os.getenv("GOOGLE_SEARCH_API_KEY") # Google Custom Search API の Key
